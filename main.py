569b2cb395a51f23935b7f6a756982d19add5838
minpeter <kali2005611@gmail.com>
2022-09-12 12:02:45 +0900

    좀 더 파이썬답게 수정

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import os
 
 input = input("Enter the ip address to send the ping to >>> ")
+
 blacklist = [" ", "&", "!", "@", "%", "^", "~", "`", "<", ">", ",", "\\", "/"]
-for i in blacklist:
-    if i in input:
-        print("Who told you..?")
-        exit()
+if any(i in blacklist for i in input):
+    print("Who told you..?")
+    exit()
+
 blacklist = ["ls", "cat", "less", "tail",
              "more", "whoami", "pwd", "busybox", "echo"]
-for i in blacklist:
-    if i in input:
-        print("Who told you..?")
-        exit()
+if any(i in input for i in blacklist):
+    print("Who told you..?")
+    exit()
 
 os.system("ping -c 1 " + input)
 print()
