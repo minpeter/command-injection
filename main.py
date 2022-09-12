7acd761d88a42fcc640a980ed5755586eb201baf
minpeter <kali2005611@gmail.com>
2022-09-12 12:06:25 +0900

    코드 단축 + 약간 단독화?

--- main.py
+++ main.py
@@ -2,14 +2,10 @@ import os
 
 input = input("Enter the ip address to send the ping to >>> ")
 
-blacklist = [" ", "&", "!", "@", "%", "^", "~", "`", "<", ">", ",", "\\", "/"]
-if any(i in blacklist for i in input):
-    print("Who told you..?")
-    exit()
-
-blacklist = ["ls", "cat", "less", "tail",
-             "more", "whoami", "pwd", "busybox", "echo"]
-if any(i in input for i in blacklist):
+if any(i in input for i in ["ls", "cat", "less", "tail", "more",
+                            "whoami", "pwd", "busybox", "echo",
+                            " ", "&", "!", "@", "%", "^", "~", "`",
+                            "<", ">", ",", "\\", "/"]):
     print("Who told you..?")
     exit()
 
